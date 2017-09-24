e16685a496f61d50a05bbae87a2718994ba3c56f
matueranet <git@daan.matuera.net>
2017-09-24 20:25:49 +0200

    First commit of FB Messenger plugin.

---
+++ README.md
@@ -0,0 +1,1 @@
+A genie-router plugin that enables the use of Facebook Messenger as a client.
---
+++ index.js
@@ -0,0 +1,4 @@
+/* Returns an object with a client and a http attribute. */
+const httpAndClient = require('./lib/messenger')
+
+module.exports = httpAndClient
